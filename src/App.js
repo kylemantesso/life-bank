8f7e74b14efe64b8e7622e3948317b7b629e0367
Kyle Mantesso <kylemantesso@gmail.com>
2020-10-29 23:55:58 +1100

    Add as-exports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Grommet} from "grommet";
+import { Grommet } from "grommet";
 import { theme } from "./theme";
 
 import "./App.css";
 import { Chatbot } from "./components/screens/Chatbot/Chatbot";
 import { Dashboard } from "./components/screens/Dashboard/Dashboard";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { ToastProvider } from 'react-toast-notifications';
-import {Login} from "./components/screens/Login/Login";
+import { ToastProvider } from "react-toast-notifications";
+import { Login } from "./components/screens/Login/Login";
 
 export const App = () => {
   return (
